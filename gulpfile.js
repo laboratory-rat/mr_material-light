51edcffd1f9bd11263083f6ec75491bdce4a5838
Oleh Tymofieiev <oleg.timofeev20@gmail.com>
2018-09-25 22:54:04 +0300

    Added hide

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task(('style'), () => {
-    return gulp.src('./lib/scss/mr_grid.scss')
+    return gulp.src('./lib/mr_grid.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/style'));
 });
 
 gulp.task('style:watch', () => {
-    gulp.watch('./lib/scss/**/*.scss', ['style']);
+    gulp.watch('./lib/**/*.scss', ['style']);
 });
