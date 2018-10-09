d477623759f501bd17f81fa30ee73f9629bf5864
Oleg Timofeev <oleg.timofeev20@gmail.com>
2018-10-09 16:16:04 +0300

    Added animations

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,12 @@ gulp.task(('min'), () => {
         .pipe(gulp.dest('./dist/style'));
 });
 
+gulp.task(('min:index'), () => {
+    return gulp.src('./web/assets/styles.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./web/assets'));
+})
+
 gulp.task('watch', () => {
     gulp.watch('./lib/**/*.scss', ['min']);
 });
